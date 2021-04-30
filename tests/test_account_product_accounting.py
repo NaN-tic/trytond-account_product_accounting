a7475bd259c58d13988520b86fb4c64ae5b5afb1
Raimon Esteve <raimonesteve@gmail.com>
2021-04-30 11:24:05 +0200

    Check test when get used taxes/accounts from product or category #044335

--- tests/test_account_product_accounting.py
+++ tests/test_account_product_accounting.py
@@ -37,6 +37,7 @@ class AccountProductAccountingTestCase(ModuleTestCase):
             account_expense, = Account.search([
                     ('type.expense', '=', True),
                     ])
+            account_expense2, = Account.copy([account_expense])
             account_revenue, = Account.search([
                     ('type.revenue', '=', True),
                     ])
@@ -44,11 +45,18 @@ class AccountProductAccountingTestCase(ModuleTestCase):
             account_tax, = Account.search([
                     ('name', '=', 'Main Tax'),
                     ])
-            tax, = Tax.create([{
+            tax, tax2 = Tax.create([{
                     'name': 'Tax 1',
                     'description': 'Tax 1',
                     'type': 'percentage',
                     'rate': Decimal('.10'),
+                    'invoice_account': account_tax.id,
+                    'credit_note_account': account_tax.id,
+                    }, {
+                    'name': 'Tax 2',
+                    'description': 'Tax 2',
+                    'type': 'percentage',
+                    'rate': Decimal('.20'),
                     'invoice_account': account_tax.id,
                     'credit_note_account': account_tax.id,
                     }])
@@ -63,25 +71,53 @@ class AccountProductAccountingTestCase(ModuleTestCase):
                 )
             template.save()
 
+            # check that has not account and taxes
             with self.assertRaises(UserError):
                 template.account_expense_used
 
-            # with account on category
-            category = ProductCategory(name='test account used',
-                account_expense=account_expense, accounting=True)
-            category.save()
-            template.account_category = category
-            template.accounts_category = True
+            self.assertEqual(len(template.customer_taxes_used), 0)
+
+            # set account and taxes on product
+            template.customer_taxes = [tax2]
+            template.account_expense = account_expense2
             template.save()
 
+            # check that product has account and taxes
+            self.assertEqual(template.account_expense_used, account_expense2)
+            self.assertEqual(len(template.customer_taxes_used), 1)
+            self.assertEqual(template.customer_taxes_used[0], tax2)
+
+            # with account on category
+            category = ProductCategory(
+                name='test account used',
+                accounting=True,
+                account_expense=account_expense,
+                customer_taxes=[tax],
+                )
+            category.save()
+
+            # set accounts and taxes from category
+            template.account_category = category
+            template.accounts_category = True
+            template.taxes_category = True
+            template.save()
+
+            # check that product has account and taxes
             self.assertEqual(template.account_expense_used, account_expense)
+            self.assertEqual(len(template.customer_taxes_used), 1)
+            self.assertEqual(template.customer_taxes_used[0], tax)
 
             # with account on grant category
-            parent_category = ProductCategory(name='parent account used',
-                account_expense=account_expense, accounting=True)
+            parent_category = ProductCategory(
+                name='parent account used',
+                accounting=True,
+                account_expense=account_expense,
+                customer_taxes=[tax],
+                )
             parent_category.save()
             category.account_expense = None
             category.account_parent = True
+            category.taxes_parent = True
             category.parent = parent_category
             category.save()
 
@@ -108,8 +144,8 @@ class AccountProductAccountingTestCase(ModuleTestCase):
             template.account_category = category
             template.accounts_category = False
             template.account_revenue = account_revenue
+            template.account_expense = account_expense
             template.taxes_category = False
-            template.customer_taxes = [tax]
             template.save()
 
             self.assertEqual(template.account_revenue_used, account_revenue)
